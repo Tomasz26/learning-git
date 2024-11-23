ec2ce97d7307d3e8676aebb2efcfe62554523cd5
Tomasz Mantur <tomasz.mantur26@gmail.com>
2024-11-23 22:44:30 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+shopping_dict = {
+    "warzywniak": ['marchew', 'seler', 'rukola'],
+    "piekarnia": ['chleb', 'pączek', 'bułki']
+}
+
+i = 0
+
+print("Lista zakupów")
+
+for store, things in shopping_dict.items():
+    capitalized_things = [item.capitalize() for item in things]
+    print(f"Idę do {store.capitalize()}, kupuję tu następujące rzeczy: {capitalized_things}")
+    i += len(things)
+
+print(f"W sumie kupuję {i} produktów")
